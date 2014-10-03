47ce401403481b9e8837ab41e0b5a9c02f5d1ddd
jmesgrant <jmesgrant@gmail.com>
2014-10-03 10:13:42 -0700

    rules

--- Stec-client/src/main/java/com/cisco/tbd/stec/client/RuleRequester.java
+++ Stec-client/src/main/java/com/cisco/tbd/stec/client/RuleRequester.java
@@ -19,23 +19,29 @@ import java.io.BufferedWriter;
 import java.io.Writer;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import org.json.simple.JSONObject;
 
 /**
  *
  * @author matetukacs
  */
 public class RuleRequester extends TimerTask {
+    
+    private int lineCount;
 
     public static void runWithFrequency(int milis) {
         Timer timer = new Timer();
         timer.schedule(new RuleRequester(), 0, milis);
     }
-    
+    private PrintWriter writer;
+    private PrintWriter writerRules;
     public void run() {
         try {
             JSONArray newRules; //        System.out.println("Hello from a thread!");
             newRules = ServerConnection.pullNewRules(FileUtils.getLastLineOfFile(Runner.PATH_TO_TIME_STAMP_FILE));
-            
+            writer = new PrintWriter("__just_log.txt", "UTF-8");
+            writerRules = new PrintWriter("somesome.txt", "UTF-8");
          updateLocalRulesWith(newRules);   
             
         } catch (IOException ex) {
@@ -45,21 +51,52 @@ public class RuleRequester extends TimerTask {
         }
     }
     
+    private int rule_counter = 200000;
+
+    public String create_requested_Snort_format(String in_ip)
+
+    {
+
+        //alert tcp any any -> any 12345 (msg:"12345 alert!!!"; sid:13400384;)
+
+        this.rule_counter++;
+
+        String res = "block tcp "+in_ip+" any -> any any (msg:\"Dynamic rule="+rule_counter+"\"; sid:"+rule_counter+";)";
+
+        writer.println("New generated:"  +res);
+
+        writer.flush();
+
+        return res;
+
+    }
+    
     public void updateLocalRulesWith(JSONArray rules) {
-        //create rules in right format and append to right file. possible throgh a static method in fileutisl. there is example code as well.            //FileWriter fWriter = new FileWriter(Runner.PATH_TO_TIME_STAMP_FILE);
-        String rulesWritten = rules.toString();
-        int startArrayIndex = 0;
-        int endArrayIndex = 0;
-        while(endArrayIndex <= rulesWritten.length()) {
-            startArrayIndex = rulesWritten.indexOf("{", endArrayIndex) + 1;
-            endArrayIndex = rulesWritten.indexOf("}", endArrayIndex);
-            String output = rulesWritten.substring(startArrayIndex, endArrayIndex);
-            try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("log.txt", true)))) {
-                out.println(output);
-            } catch (IOException e) {
-                //exception handling left as an exercise for the reader
-            }
-            //fWriter.write(output+"/n");
+        //create rules in right format and append to right file. possible through a static method in fileutisl. there is example code as well.            //FileWriter fWriter = new FileWriter(Runner.PATH_TO_TIME_STAMP_FILE);
+       // String rulesWritten = rules.toString();
+        //      rules['timestamp'] - int 
+        //      rules['data'] - array
+        for (Object object : rules) {
+            JSONObject p = (JSONObject) object;
+            //System.out.println(object.toString());
+            //System.out.println(p.get("ip").toString());
+            String temp = create_requested_Snort_format(p.get("ip").toString());
+            writerRules.println(temp);
+            writerRules.flush();
         }
+        /*
+        Iterator<String> iterator = rules.iterator();
+
+        while (iterator.hasNext()) {
+
+        //System.out.println(iterator.next().toString());
+        }
+    
+        */
+          //  try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("./../log.txt", true)))) {
+          //      out.println(output);
+          //  } catch (IOException e) {
+                //exception handling left as an exercise for the reader
+           // }
     }       
 }
--- Stec-client/src/main/java/com/cisco/tbd/stec/client/ServerConnection.java
+++ Stec-client/src/main/java/com/cisco/tbd/stec/client/ServerConnection.java
@@ -91,6 +91,7 @@ public class ServerConnection {
 
         
         JSONObject json = (JSONObject)new JSONParser().parse(br.readLine());
+        //System.out.println(json.toString());
         String newTimeStamp = json.get("timestamp").toString();
         JSONArray rules = (JSONArray) json.get("data");
         
