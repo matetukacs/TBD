3a2387e681130bbe576b5c665df91da385a44e54
Mate Tukacs <mtukacs@cisco.com>
2014-10-02 17:10:39 -0700

    added log file

---
+++ Stec-client/src/main/java/com/cisco/tbd/stec/client/Runner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.cisco.tbd.stec.client;
+
+/**
+ *
+ * @author matetukacs
+ */
+public class Runner {
+
+    public static void main(String[] args) throws Exception{
+        System.err.println("Specify output folder in mtukuacs/output");
+    }
+}
